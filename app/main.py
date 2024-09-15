4fcc15b8babdde6d85815f32732ae4f2c4a5d008
Mohammed Alessi <ma.al_essi@icloud.com>
2024-09-15 15:28:33 +0300

    add: parent node

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
+from htmlnode import LeafNode, ParentNode
 from textnode import TextNode
 
 if __name__ == "__main__":
-    text_node = TextNode("some text", "bold", None)
-    print(text_node)
+    node = ParentNode(
+        "p",
+        [
+            LeafNode(tag="b", value="Bold text"),
+            LeafNode(tag=None, value="Normal text"),
+            LeafNode(tag="i", value="italic text"),
+            LeafNode(tag=None, value="Normal text"),
+        ],
+    )
+    print(node.to_html())
--- app/test_htmlnode.py
+++ app/test_htmlnode.py
@@ -32,3 +32,55 @@ class TestLeafNode(unittest.TestCase):
         node = LeafNode(None)
         with self.assertRaises(ValueError):
             node.to_html()
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        simple_node = ParentNode(
+            "p",
+            [
+                LeafNode(tag="b", value="Bold text"),
+                LeafNode(tag=None, value="Normal text"),
+                LeafNode(tag="i", value="italic text"),
+                LeafNode(tag=None, value="Normal text"),
+            ],
+        )
+
+        self.assertEqual(simple_node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+
+        complex_node = ParentNode(
+            "p",
+            [
+                ParentNode(
+                    "ul",
+                    [
+                        LeafNode("Cofee", "li", probs={"class": "item"}),
+                        LeafNode("Tea", "li", probs={"class": "item"}),
+                        LeafNode("Milk", "li", probs={"class": "item"}),
+                    ],
+                ),
+                LeafNode(tag="b", value="Bold text"),
+                LeafNode(tag=None, value="Normal text"),
+                LeafNode(tag="i", value="italic text"),
+                LeafNode(tag=None, value="Normal text"),
+            ],
+        )
+
+        correct_html = '<p><ul><li class="item">Cofee</li><li class="item">Tea</li><li class="item">Milk</li></ul><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+        self.assertEqual(complex_node.to_html(), correct_html)
+
+    def to_html_with_probs(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode(tag="b", value="Bold text"),
+                LeafNode(tag=None, value="Normal text"),
+                LeafNode(tag="i", value="italic text"),
+                LeafNode(tag=None, value="Normal text"),
+            ],
+            probs={"id": "body", "class": "text"},
+        )
+
+        self.assertEqual(
+            node.to_html(), '<p id="body" class="text"><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+        )
