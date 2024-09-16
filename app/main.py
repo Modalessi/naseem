293c3c56d2f1c9c067fdf439f3eb302ab7c23566
Mohammed Alessi <ma.al_essi@icloud.com>
2024-09-17 01:34:42 +0300

    add: copy functionlaity

--- app/main.py
+++ app/main.py
@@ -1,32 +1,43 @@
+import os
+import shutil
+
 from block_md import *
 from enums import TextType
 from inline_md import split_nodes_delimiter, text_to_text_nodes
 from textnode import TextNode
 
 
+def copy_contents(src_dir: str, dest_dir: str):
+
+    # delete public contents
+    shutil.rmtree("public")
+    os.mkdir("public")
+
+    contents = copy_content_helper(src_dir)
+
+    for file in contents:
+        print(f"[ COPIED ] {file.name}")
+
+
+def copy_content_helper(path: str):
+    entites = os.scandir(path)
+    contents = []
+
+    for entity in entites:
+        entity_relative_path = entity.path.split("/", 1)[1]
+        if entity.is_file():
+            shutil.copyfile(entity.path, os.path.join("public", entity_relative_path))
+            contents.append(entity)
+            continue
+
+        os.mkdir(os.path.join("public", entity_relative_path))
+        contents.extend(copy_content_helper(entity.path))
+
+    return contents
+
+
 def main():
-    md = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
-* This is the **first** list item in a list block
-* This is a list item
-* This is another list item"""
-
-    md_in_html = """<div>
-<h1>This is a heading</h1>
-
-<p>This is a paragraph of text. It has some <b>bold</b> and <i>italic</i> words inside of it.</p>
-
-<ul>
-<li>This is the <b>first</b> list item in a list block</li>
-<li>This is a list item</li>
-<li>This is another list item</li>
-</ul>
-</div>"""
-
-    html_node = md_to_html_node(md)
-    print(html_node.to_html())
+    copy_contents("static", "public")
 
 
 if __name__ == "__main__":
